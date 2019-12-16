a16a5ea8df93bd580902d499d97b6e53024d229c
Morgan Schuler <morganlschuler@gmail.com>
2019-12-16 15:53:42 -0800

    created react routers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,87 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">That's showbiz, baby!</h1>
+//         </header>
+//         <p className="App-intro">
+//            <p>Search</p> 
+//            <p>Rental Library</p> 
+//            <a>Customers</a> 
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/rentals">Rental List</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/rentals">
+            <Rentals />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
+}
+function Rentals() {
+  return <h2>Rentals</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+
+// export default App;
