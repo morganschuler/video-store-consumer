263530fcf3dc841a321f22f84fd048c7c21e018d
ohcloud <cloudylopez@icloud.com>
2019-12-17 13:00:33 -0800

    created customerlist and movie lsit components. began scaffolding of app.

--- src/App.js
+++ src/App.js
@@ -8,23 +8,34 @@ import {
 import logo from './logo.svg';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">That's showbiz, baby!</h1>
-//         </header>
-//         <p className="App-intro">
-//            <p>Search</p> 
-//            <p>Rental Library</p> 
-//            <a>Customers</a> 
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    movies: [], //should be an empty array
+    rentalCustomer: [], //not sure what goes here
+    rentalMovie: [], //not sure what goes here
+  }
+}
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">That's showbiz, baby!</h1>
+        </header>
+        <p className="App-intro">
+           <p>Search</p> 
+           <p>Rental Library</p> 
+           <a>Customers</a> 
+        </p>
+      </div>
+    );
+  }
+}
 
 export default function App() {
   return (
