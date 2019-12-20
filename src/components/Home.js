49680d347d6a4aa58edf2de99b25341fe4b4df77
Morgan Schuler <morganlschuler@gmail.com>
2019-12-19 23:26:05 -0800

    completed styling

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+
+render(){
+  return(
+    <div className="homepage">
+      <img className="Homepage" src="https://i.pinimg.com/originals/59/c7/eb/59c7ebe899eb39f542892742c2c76925.jpg"></img>
+</div>
+  )
+}
+}
+export default Home;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+import './Movie.css'
 
 const Movie = (props) => {
   // const onMovieSelect = (event) => {
@@ -7,19 +11,47 @@ const Movie = (props) => {
   //   this.props.onMovieSelectCallback(movie)
   // }
   return (
-    <div>
-      <h3>{props.title}</h3>
-      <p>{props.overview}</p>
-      <p>Release date: {props.release_date}</p>
-      <img src={props.image_url}/>
-      <button
-        type="button"
-        onClick={() => {props.findMovie(props.id)}}>
-          {console.log(props.id)}
-      Select Me
-      </button>
-    </div>
-  )
+  
+  // <span className="container">
+  //   <div className="movie">
+  //   <h3 className="title">{props.title}</h3>
+  //     <img className="image" src={props.image_url}/>
+  //     <div className="content">{props.overview} 
+  //     <br></br>
+  //     Release Date: {props.release_date}</div>
+  //     <button className="button"
+  //       type="button"
+  //       onClick={() => {props.findMovie(props.id)}}>
+  //         {console.log(props.id)}
+  //     Rent Me!
+  //     </button>
+
+  //   </div>
+  // </span>
+  <ul>
+  <Card style={{ width: "20rem" }}>
+          <Card.Header>
+            <Card.Img className="img" variant="top" src={props.image_url} />
+            <Card.Title className="movie-title">{props.title}</Card.Title>
+            <Button
+              variant="warning"
+              type="button"
+              onClick={() => {props.findMovie(props.id)}}>
+            Rent Me!
+            </Button>
+          </Card.Header>
+        </Card>
+        <Card style={{ width: "20rem" }}>
+            <Card.Body>
+              <Card.Text>{props.overview}</Card.Text>
+              <Card.Text>Release Date: {props.release_date}</Card.Text>
+            </Card.Body>
+        </Card>
+    </ul>
+  );
+
 }
 
 export default Movie; 
+
+
