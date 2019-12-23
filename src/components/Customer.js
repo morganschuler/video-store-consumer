6a94e99d3277039d9a4adc09d0dc97d4ed0339fd
Morgan Schuler <morganlschuler@gmail.com>
2019-12-22 18:41:01 -0800

    updated customer

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,23 +1,52 @@
-import React from 'react'
+// import React from 'react'
 
+// const Customer = (props) => {
+//   // const onMovieSelect = (event) => {
+//   //   const movie = props.title;
+
+//   //   this.props.onMovieSelectCallback(movie)
+//   // }
+//   return (
+//     <div>
+//       <h3>{props.name}</h3>
+
+//       <button
+//         type="button"
+//         onClick={() => {props.findCustomer(props.id)}}>
+//           {console.log(props.id)}
+//       Select Me
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default Customer; 
+
+import React from 'react';
+import { Table } from 'reactstrap';
+import "./CustomerList.css";
 const Customer = (props) => {
-  // const onMovieSelect = (event) => {
-  //   const movie = props.title;
-
-  //   this.props.onMovieSelectCallback(movie)
-  // }
   return (
     <div>
-      <h3>{props.name}</h3>
-
-      <button
-        type="button"
-        onClick={() => {props.findCustomer(props.id)}}>
-          {console.log(props.id)}
-      Select Me
-      </button>
+      <Table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th className="name">Name</th>
+          <th className="account-credit">Account Credit</th>
+          <th className="phone">Phone Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">{props.id}</th>
+          <td className="table-name" onClick={() => {props.findCustomer(props.id)}}>{props.name}</td>
+          <td className="table-credit"onClick={() => {props.findCustomer(props.id)}}>{props.account_credit}</td>
+      <td className="table-phone" onClick={() => {props.findCustomer(props.id)}}>{props.phone}</td>
+        </tr>
+        </tbody>
+      </Table>
     </div>
-  )
+  );
 }
-
-export default Customer; 
+export default Customer;
